5c720661cc289d4e91460bfe91761e5f8e984919
Harjas <harjassingh_co20b4_07@dtu.ac.in>
2022-12-26 19:09:25 +0530

    Product Page Completed

---
+++ e-commerce/context/StateContext.js
@@ -0,0 +1,72 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
+
+const Context = createContext();
+
+export const StateContext = ({ children }) => {
+  const [showCart, setShowCart] = useState(false);
+
+  const [cartItems, setCartItems] = useState([]);
+  const [totalPrice, setTotalPrice] = useState();
+  const [qty, setQty] = useState(1);
+  const [totalQuantities, setTotalQuantities] = useState();
+
+  const onAdd = (product, quantity) => {
+    const checkProductInCart = cartItems.find(
+      (item) => item._id === product._id
+    );
+
+    setTotalPrice(
+      (prevTotalPrice) => prevTotalPrice + product.price * quantity
+    );
+
+    setTotalQuantities((prevTotalQuantities) => prevTotalQuantities + quantity);
+
+    if (checkProductInCart) {
+      const updatedCartItems = cartItems.map((cartProducts) => {
+        if (cartProducts._id === product._id)
+          return {
+            ...cartProducts,
+            quantity: cartProducts.quantity + quantity,
+          };
+      });
+
+      setCartItems(updatedCartItems);
+    } else {
+      product.quantity = quantity;
+
+      setCartItems([...cartItems, { ...product }]);
+    }
+    toast.success(`${qty} ${product.name} added to the cart.`);
+  };
+  const incQty = () => {
+    setQty((prevQty) => prevQty + 1);
+  };
+
+  const decQty = () => {
+    setQty((prevQty) => {
+      if (prevQty - 1 < 1) return 1;
+
+      return prevQty - 1;
+    });
+  };
+  return (
+    <Context.Provider
+      value={{
+        showCart,
+        cartItems,
+        totalPrice,
+        qty,
+        totalQuantities,
+        incQty,
+        decQty,
+        onAdd,
+        setShowCart,
+      }}
+    >
+      {children}
+    </Context.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(Context);
